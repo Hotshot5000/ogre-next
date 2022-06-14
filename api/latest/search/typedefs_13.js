b0f0474595545ebd9d0a97249ce40e1fe138748e
Hotshot5000 <Hotshot5000@users.noreply.github.com>
2022-06-14 18:22:28 +0000

    deploy: 43defabaff375bb9e070f13cec88b99554e397bc

---
+++ api/latest/search/typedefs_13.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['uavbufferpackedvec_0',['UavBufferPackedVec',['../group___effects.html#ga7502fc233f843b0f0c1b0fc2aa26099b',1,'Ogre']]],
+  ['uavdependencyvec_1',['UavDependencyVec',['../class_ogre_1_1_compositor_pass_def.html#a114c9c278a29353ebe78cc7d7e3fce6f',1,'Ogre::CompositorPassDef']]],
+  ['uchar_2',['uchar',['../namespace_ogre.html#a07f1f7c1d84d58c9beae02e1ebb945e4',1,'Ogre']]],
+  ['uint_3',['uint',['../namespace_ogre.html#ab6e842a18caf062ed73a5d3badd0dd4c',1,'Ogre']]],
+  ['ulong_4',['ulong',['../namespace_ogre.html#a4304981347643708ee9a7ba8884e5758',1,'Ogre']]],
+  ['unaryoptionlist_5',['UnaryOptionList',['../group___general.html#ga1fdbb1354b0aef3b66cfb0f34b8d5fde',1,'Ogre']]],
+  ['uniquevertexset_6',['UniqueVertexSet',['../struct_ogre_1_1_lod_data.html#a9e26fcf32eabb9261a14ff8b90cf9d63',1,'Ogre::LodData']]],
+  ['unsafeblockvec_7',['UnsafeBlockVec',['../class_ogre_1_1_metal_discard_buffer_manager.html#a3651ee834a507f877021ffcdebc01734',1,'Ogre::MetalDiscardBufferManager::UnsafeBlockVec()'],['../class_ogre_1_1_vulkan_discard_buffer_manager.html#ab1c67584902d5c96beac8796eeb98c1e',1,'Ogre::VulkanDiscardBufferManager::UnsafeBlockVec()']]],
+  ['unsignedintconstantlist_8',['UnsignedIntConstantList',['../group___materials.html#gab353aaf9b52a6f32e2f81985364b2426',1,'Ogre']]],
+  ['ushort_9',['ushort',['../namespace_ogre.html#af851e71354d841df7138f9877c6f5931',1,'Ogre']]],
+  ['uvrect_10',['UVRect',['../class_ogre_1_1_font.html#a17ebc3668a45365c1243a83f36a512ef',1,'Ogre::Font']]]
+];
